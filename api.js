da3cc6e79705d9bd2688b300b2a967c26a452b60
iriama <iriama@protonmail.com>
2020-02-19 16:41:15 +0100

    initial commit

---
+++ api.js
@@ -0,0 +1,253 @@
+const request = require('request');
+
+jsonedCookie = {};
+
+function parseSetCookie(setCookie) {
+
+    for (let cookie of setCookie) {
+        const e0 = cookie.split(';')[0];
+
+        const ss = e0.split['='];
+        const matches = e0.match(/^(.+)\=(.+)/);
+
+        const key = matches[1];
+        const val = matches[2];
+
+        jsonedCookie[key] = val;
+    }
+
+}
+
+function getCookie() {
+
+    let cookie = "";
+
+    Object.keys(jsonedCookie).forEach(key => {
+
+        if (cookie !== "") cookie += '; ';
+
+        cookie += `${key}=${jsonedCookie[key]}`;
+
+    });
+
+    return cookie;
+}
+
+function getLocationAutocompleteV1(locale, address) {
+
+    console.log('https://www.ubereats.com/api/getLocationAutocompleteV1?localeCode=' + locale);
+
+    const options = {
+        url: 'https://www.ubereats.com/api/getLocationAutocompleteV1?localeCode=' + locale,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'x-csrf-token': 'x',
+            'Cookie': getCookie()
+        },
+        json: {"query": address}
+    }
+
+    return new Promise((resolve, reject) => {
+    
+        request(options,(err, resp, body) => {
+            if (err || body.status !== 'success') {
+                reject(err);
+                return;
+            }
+            parseSetCookie(resp.headers['set-cookie']);
+        
+            resolve(body.data[0]);
+        });
+
+    });
+}
+
+
+function getLocationDetailsV1(locale, location) {
+
+    console.log('https://www.ubereats.com/api/getLocationDetailsV1?localeCode=' + locale);
+
+    const options = {
+        url: 'https://www.ubereats.com/api/getLocationDetailsV1?localeCode=' + locale,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'x-csrf-token': 'x',
+            'Cookie': getCookie()
+        },
+        json: location
+    }
+
+    return new Promise((resolve, reject) => {
+    
+        request(options,(err, resp, body) => {
+            if (err || body.status !== 'success') {
+                reject(err);
+                return;
+            }
+            parseSetCookie(resp.headers['set-cookie']);
+            
+            resolve(body.data);
+        });
+
+    });
+}
+
+function setTargetLocationV1(locale) {
+
+    console.log('https://www.ubereats.com/api/setTargetLocationV1?localeCode=' + locale);
+
+    const options = {
+        url: 'https://www.ubereats.com/api/setTargetLocationV1?localeCode=' + locale,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'x-csrf-token': 'x',
+            'Cookie': getCookie()
+        },
+        json: {}
+    }
+
+    return new Promise((resolve, reject) => {
+    
+        request(options,(err, resp, body) => {
+            if (err || body.status !== 'success') {
+                reject(err);
+                return;
+            }
+            parseSetCookie(resp.headers['set-cookie']);
+
+            resolve();
+        });
+
+    });
+}
+
+
+function getFeedV1(locale, offset=0) {
+    
+    console.log('https://www.ubereats.com/api/getFeedV1?localeCode=' + locale);
+
+    const options = {
+        url: 'https://www.ubereats.com/api/getFeedV1?localeCode=' + locale,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'x-csrf-token': 'x',
+            'Cookie': getCookie()
+        },
+        json: {
+            "cacheKey": "",
+            "feedSessionCount": {
+                "announcementCount": 0,
+                "announcementLabel": ""
+            },
+            "userQuery": "",
+            "date": "",
+            "startTime": 0,
+            "endTime": 0,
+            "carouselId": "",
+            "sortAndFilters": [],
+            "pageInfo": {
+                offset,
+                "pageSize": 80
+            }
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+    
+        request(options,(err, resp, body) => {
+            if (err || body.status !== 'success') {
+                reject(err);
+                return;
+            }
+            parseSetCookie(resp.headers['set-cookie']);
+
+            resolve(body.data);
+        });
+
+    });
+}
+
+function getStoreV1(locale, uuid) {
+    
+    console.log('https://www.ubereats.com/api/getStoreV1?localeCode=' + locale);
+
+    const options = {
+        url: 'https://www.ubereats.com/api/getStoreV1?localeCode=' + locale,
+        method: 'POST',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
+            'x-csrf-token': 'x',
+            'Cookie': getCookie()
+        },
+        json: {"storeUuid": uuid,"sfNuggetCount":0}
+    }
+
+    return new Promise((resolve, reject) => {
+    
+        request(options,(err, resp, body) => {
+            if (err || body.status !== 'success') {
+                reject(err);
+                return;
+            }
+            parseSetCookie(resp.headers['set-cookie']);
+
+            resolve(body.data);
+        });
+
+    });
+}
+
+
+function parseStores(feed) {
+
+    const stores = feed.storesMap;
+
+    const list = [];
+    
+    for (let key in stores) {
+        const store = stores[key];
+
+        list.push(store);
+    }
+
+    return list;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(() =>  resolve(), ms);
+    });
+}
+
+async function getStores(locale, location_str, delay=0) {
+    const completed =  await getLocationAutocompleteV1(locale, location_str);
+    const location = await getLocationDetailsV1(locale, completed);
+    jsonedCookie['uev2.loc'] = JSON.stringify(location);
+    await setTargetLocationV1(locale);
+    
+    let meta = { hasMore: true, offset: 0 };
+    let stores = [];
+
+    do {
+        const feed = await getFeedV1(locale, meta.offset);
+        meta = feed.meta;
+        stores = stores.concat( parseStores(feed) );
+
+        await sleep(delay);
+    } while(meta.hasMore);
+
+    return stores;
+}
+
+async function getStoreAddress(locale, storeUuid) {
+    return (await getStoreV1(locale, storeUuid)).location;
+}
+
+module.exports = {
+    getStores,
+    getStoreAddress
+}
---
+++ server.js
@@ -0,0 +1,321 @@
+const CONFIG = require('./config.json');
+const MongoClient  = require('mongodb').MongoClient;
+const assert = require('assert');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const helmet = require('helmet');
+const client = new MongoClient(CONFIG.DB_URL, { useUnifiedTopology: true });
+const moment = require('moment');
+const api = require('./api');
+
+
+const ENUM_PROCESS_STATUS = {
+  NONE: 0,
+  PROCESSING: 1
+}
+
+let process_stores = [{
+  postal: "0",
+  locale: "en-GB",
+  status: ENUM_PROCESS_STATUS.PROCESSING
+}];
+
+let process_locations =  [{
+  storeId: "0",
+  status: ENUM_PROCESS_STATUS.PROCESSING
+}];
+
+
+router.get('/status', function (req, res) {
+  res.json({
+    success: true
+  });
+});
+
+router.get(/^\/stores\/((?:[0-8]\d|9[0-8])\d{3})$/, async (req, res) => {
+
+  let locale = CONFIG.DEFAULT_LOCALE;
+  if (req.query.locale && /^[a-z]{2}\-[A-Z]{2}$/.test(req.query.locale))
+    locale = req.query.locale;
+
+  const postal = req.params[0];
+
+  if (processStatus(postal, locale) === ENUM_PROCESS_STATUS.PROCESSING) {
+    return res.json({
+      success: true,
+      data: { status: 'processing' }
+    });
+  }
+
+  const collectionName = `${CONFIG.DB_STORES_COLLECTION_PREFIX}-${locale}`;
+
+  try {
+    await client.db().createCollection(collectionName);
+  } catch(err) {
+    return res.json({
+      success: false,
+      message: 'internal error',
+      code: '1e3a'
+    });
+  }
+
+  const collection = client.db().collection(collectionName);
+  
+  try {
+
+    const docs = await collection.findOne({ postal });
+
+    if (docs && moment().diff(moment(docs.lastUpdate, 'x'), 'minutes') < CONFIG.STORES_UPDATE_MIN) {
+
+      processAssertDestroyed(postal, locale);
+
+      return res.json({
+        success: true,
+        data: { status: "finished", stores: docs.stores, age: moment().diff(moment(docs.lastUpdate, 'x'), 'minutes') }
+      });
+    }
+
+  } catch(err) {
+    processAssertDestroyed(postal, locale);
+
+    return res.json({
+      success: false,
+      message: 'internal error',
+      code: '9e7f'
+    });
+  }
+
+  // No cached version
+  process(locale, collectionName, postal);
+
+  return res.json({
+    success: true,
+    data: { status: 'processing' }
+  });
+
+});
+
+
+function processStatus(postal, locale) {
+  const process = process_stores.find(x => x.postal === postal && x.locale === locale);
+
+  if (!process) return ENUM_PROCESS_STATUS.NONE;
+
+  return process.status;
+}
+
+function locationProcessStatus(storeId) {
+  const process = process_stores.find(x => x.storeId === storeId);
+
+  if (!process) return ENUM_PROCESS_STATUS.NONE;
+
+  return process.status;
+}
+
+function processAssertDestroyed(postal, locale) {
+  process_stores = process_stores.filter(x => x.postal !== postal && x.locale === locale);
+}
+
+function locationProcessAssertDestroyed(storeId) {
+  process_stores = process_stores.filter(x => x.storeId !== storeId);
+}
+
+async function process(locale, collectionName, postal){
+  
+  const process = process_stores.find(x => x.postal === postal && x.locale === locale);
+  if (process && process.status === ENUM_PROCESS_STATUS.PROCESSING) return;
+
+  console.log(`> processing ${postal} (${locale})`);
+
+  processAssertDestroyed(postal, locale);
+
+  process_stores.push({
+    postal,
+    locale,
+    status: ENUM_PROCESS_STATUS.PROCESSING
+  });
+
+  let stores;
+  try {
+    stores = await api.getStores(locale, postal);
+  } catch (err) {
+    console.error(`> catched api error @getStores(${locale}, ${postal})`);
+    processAssertDestroyed(postal, locale);
+    return;
+  }
+
+  try {
+    stores = stores.map(s => {
+      return {
+        image: s.heroImageUrl,
+        id: s.uuid,
+        name: s.title,
+        categories: s.categories,
+        stars: s.feedback ? s.feedback.rating : 0,
+        count: s.feedback ? parseInt(s.feedback.ratingCount.replace('+', '')) : 0,
+        promotion: (s.promotion ? s.promotion.text : null),
+        open: s.isOpen,
+        nextOpen: s.closedMessage
+      }
+    });
+  } catch(err) {
+    console.error(`> catched mapping error @getStores(${locale}, ${postal})`);
+    processAssertDestroyed(postal, locale);
+    return;
+  }
+  
+  try {
+    const collection = client.db().collection(collectionName);
+
+    const docs = await collection.findOne({postal});
+
+    const lastUpdate = moment.now('x');
+
+    if (!docs)
+      await collection.insertOne({ postal, stores, lastUpdate });
+    else
+      await collection.findOneAndUpdate({ postal }, { $set: { stores, lastUpdate } });
+
+  } catch(err) {
+    console.error(`> catched db error @getStores(${locale}, ${postal})`);
+    processAssertDestroyed(postal, locale);
+    return;
+  }
+
+  console.log(`> processed ${postal} (${locale})`);
+  processAssertDestroyed(postal, locale);
+}
+
+async function locationProcess(storeId){
+
+  const process = process_locations.find(x => x.storeId === storeId);
+  if (process && process.status === ENUM_PROCESS_STATUS.PROCESSING) return;
+
+  const locale = CONFIG.DEFAULT_LOCALE;
+
+  console.log(`> location ${storeId}`);
+  
+  locationProcessAssertDestroyed(storeId);
+
+  process_locations.push({
+    storeId,
+    status: ENUM_PROCESS_STATUS.PROCESSING
+  });
+
+  let location;
+  try {
+    location = await api.getStoreAddress(locale, storeId);
+  } catch (err) {
+    console.error(`> catched api error @getStoreAddress(${storeId}})`);
+    locationProcessAssertDestroyed(storeId);
+    return;
+  }
+
+  try {
+    location =  {
+      address: location.address,
+      latitude: location.latitude,
+      longitude: location.longitude
+    };
+  } catch (err) {
+    console.error(`> catched mapping error @getStoreAddress(${storeId}})`);
+    locationProcessAssertDestroyed(storeId);
+    return;
+  }
+
+  try {
+    const collection = client.db().collection(CONFIG.DB_LOCATIONS_COLLECTION);
+
+    const docs = await collection.findOne({storeId});
+
+    const lastUpdate = moment.now('x');
+
+    if (!docs)
+      await collection.insertOne({ storeId, location, lastUpdate });
+    else
+      await collection.findOneAndUpdate({ storeId }, { $set: { location, lastUpdate } });
+
+  } catch(err) {
+    console.error(`> catched db error @getStoreAddress(${storeId})`);
+    locationProcessAssertDestroyed(storeId);
+    return;
+  }
+
+  console.log(`> located ${storeId}`);
+  locationProcessAssertDestroyed(storeId);
+
+}
+
+router.get(/^\/locate\/([0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12})$/, async (req, res) => {
+  
+  const storeId = req.params[0];
+
+  if (locationProcessStatus(storeId) === ENUM_PROCESS_STATUS.PROCESSING) {
+    return res.json({
+      success: true,
+      data: { status: 'processing' }
+    });
+  }
+
+  try {
+    await client.db().createCollection(CONFIG.DB_LOCATIONS_COLLECTION);
+  } catch(err) {
+    return res.json({
+      success: false,
+      message: 'internal error',
+      code: '55e2'
+    });
+  }
+  
+
+  const collection = client.db().collection(CONFIG.DB_LOCATIONS_COLLECTION);
+  
+  try {
+
+    const docs = await collection.findOne({ storeId });
+
+    if (docs && moment().diff(moment(docs.lastUpdate, 'x'), 'days') < CONFIG.LOCATIONS_UPDATE_DAYS) {
+
+      locationProcessAssertDestroyed(storeId);
+
+      return res.json({
+        success: true,
+        data: { status: "finished", location: docs.location, age: moment().diff(moment(docs.lastUpdate, 'x'), 'days') }
+      });
+    }
+
+  } catch(err) {
+    locationProcessAssertDestroyed(storeId);
+
+    return res.json({
+      success: false,
+      message: 'internal error',
+      code: '78ef'
+    });
+  }
+
+  // No cached version
+  locationProcess(storeId);
+
+  return res.json({
+    success: true,
+    data: { status: 'processing' }
+  });
+
+
+});
+
+app.use(helmet());
+app.use('/api', router);
+
+client.connect((err) => {
+  assert.equal(null, err);
+
+  console.log('> connected to database');
+
+  app.listen(CONFIG.PORT, () => console.log(`> up and running on port ${CONFIG.PORT}`));
+});
+
+
+
